90f1e293b3a6968e3d200a4b619706a741dab89f
Lorenah2022 <116637604+Lorenah2022@users.noreply.github.com>
2024-05-16 12:57:19 +0200

    #3 move method getFrequentRenterPoints from Rental to Movie

--- src/main/java/ubu/gii/Movie.java
+++ src/main/java/ubu/gii/Movie.java
@@ -56,4 +56,11 @@ public class Movie {
 		}
 		return result;
 	}
+
+	int getFrequentRenterPoint(int daysRented) {
+		int frequentRenterPoints = 1;
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
 }
