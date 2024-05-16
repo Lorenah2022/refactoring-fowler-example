45a4cdd224ea2369444d996df5e9b5299eb6960d
Lorenah2022 <116637604+Lorenah2022@users.noreply.github.com>
2024-05-16 13:33:32 +0200

    #3 move method  getFrequentRenterPoint from Movie to Price

--- src/main/java/ubu/gii/Movie.java
+++ src/main/java/ubu/gii/Movie.java
@@ -55,9 +55,6 @@ public class Movie {
 	}
 
 	int getFrequentRenterPoint(int daysRented) {
-		int frequentRenterPoints = 1;
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
+		return _priceCode.getFrequentRenterPoint(daysRented);
 	}
 }
--- src/main/java/ubu/gii/Price.java
+++ src/main/java/ubu/gii/Price.java
@@ -4,4 +4,11 @@ public abstract class Price {
 	public abstract int getPriceCode();
 	
 	public abstract double getCharge(int daysRented);
+
+	int getFrequentRenterPoint(int daysRented) {
+		int frequentRenterPoints = 1;
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
 }
